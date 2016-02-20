fd2de01d19550ad064215c234d61976cb4205335
22a <meehanp2@tcd.ie>
2016-02-20 19:11:28 +0000

    naive blacklisting

--- proxy.js
+++ proxy.js
@@ -4,20 +4,31 @@ var http = require('http'),
     url = require('url');
 
 var err_esc_red = '\x1B[31m';
+var err_esc_green = '\x1B[32m';
 var err_esc_black = '\x1B[0m';
+
+var blacklist = new Set();
+blacklist.add('http://hire.meehan.co/');
 
 var proxy = httpProxy.createServer({ws: true, prependPath: false});
 
 var server = http.createServer(function (req, res) {
-  console.log('Req for: ' + req.url);
-
-  proxy.web(req, res, {target: req.url, secure: false},
-            function(e){
-              if(e){
-                console.error(err_esc_red + 'Something bad happened, let\'s not worry about it just yet...' + err_esc_black)
+  if (blacklist.has(req.url)){
+    console.log(err_esc_green + 'Blocked request to blacklisted site: ' + req.url + err_esc_black);
+    res.writeHead(401, {'Content-Type': 'text/plain'});
+    res.write('Access to blacklisted site denied.\n' + req.url + '\n');
+    res.end();
+  }
+  else {
+    console.log('Req for: ' + req.url);
+    proxy.web(req, res, {target: req.url, secure: false},
+              function(e){
+                if(e){
+                  console.error(err_esc_red + 'Something bad happened, let\'s not worry about it just yet...' + err_esc_black)
+                }
               }
-            }
-           );
+             );
+  }
 }).listen(8080);
 
 server.on('connect', function (req, socket) {
