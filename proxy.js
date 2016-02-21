cd32d11176a0b576b0444de2e785de295052d271
22a <meehanp2@tcd.ie>
2016-02-21 03:07:34 +0000

    beautiful live request console

--- proxy.js
+++ proxy.js
@@ -7,6 +7,18 @@ var err_esc_red = '\x1B[31m';
 var err_esc_green = '\x1B[32m';
 var err_esc_black = '\x1B[0m';
 
+var ws = require('ws');
+var WebSocketServer = ws.Server
+  , wss = new WebSocketServer({ port: 8008 });
+
+wss.on('connection', function connection(websocket) {
+  websocket.send('Connected to Node Proxy Websocket Console - Peter Meehan - 13318021');
+});
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
 
 var express = require('express');
 var app = express();
@@ -43,24 +55,27 @@ var proxy = httpProxy.createServer({ws: true, prependPath: false});
 
 var server = http.createServer(function (req, res) {
   if (blacklist.has(req.url)){
-    console.log(err_esc_green + 'Blocked request to blacklisted site: ' + req.url + err_esc_black);
+    //console.log(err_esc_green + 'Blocked request to blacklisted site: ' + req.url + err_esc_black);
+    wss.broadcast('Blocked request to blacklisted site: ' + req.url);
     res.writeHead(401, {'Content-Type': 'text/plain'});
     res.write('Access to blacklisted site denied.\n' + req.url + '\n');
     res.end();
   }
   else {
-    console.log('Req for: ' + req.url);
+    //console.log('Req for: ' + req.url);
+    wss.broadcast(req.url);
     proxy.web(req, res, {target: req.url, secure: false}, function(e){
       // proxy module error logging
       if(e){
-        console.error(err_esc_red + 'Something bad happened, let\'s not worry about it just yet...' + err_esc_black)
+        //console.error(err_esc_red + 'Something bad happened, let\'s not worry about it just yet...' + err_esc_black)
       }
     });
   }
 }).listen(8080);
 
 server.on('connect', function (req, socket) {
-  console.log('Con req for:' + req.url);
+  //console.log('Con req for:' + req.url);
+  wss.broadcast(req.url);
   var serverUrl = url.parse('https://' + req.url);
   var srvSocket = net.connect(serverUrl.port, serverUrl.hostname, function() {
     socket.write('HTTP/1.1 200 Connection Established\r\n' + '\r\n');
@@ -71,7 +86,7 @@ server.on('connect', function (req, socket) {
 
 // explicit tcp error handling
 process.on('uncaughtException', function (e) {
-  if (e.stack.includes('ECONNRESET') || e.stack.includes('ECONNREFUSED')){
+  if (e.stack.includes('ECONNRESET') || e.stack.includes('ECONNREFUSED') || e.stack.includes('ENOTFOUND')) {
     console.error(err_esc_red + 'TCP ' + e.stack.split('\n')[0] + err_esc_black);
   }
   else{
--- public/app.js
+++ public/app.js
@@ -12,7 +12,29 @@ var nodeProxy = angular.module('nodeProxy', ['ngRoute'])
   $locationProvider.html5Mode(true);
 }])
 
+.directive('showTail', function () {
+  return function (scope, elem, attr) {
+    scope.$watch(function () {
+      return elem[0].value;
+    },
+    function (e) {
+      elem[0].scrollTop = elem[0].scrollHeight;
+    });
+  }
+})
+
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.messageLog = '';
+  $scope.url = {};
+  $scope.url.protocol = 'http://';
+
+  var proxySocket = new WebSocket("ws://localhost:8008");
+  proxySocket.onmessage = function (event) {
+    $scope.$apply(function(){
+      $scope.messageLog = $scope.messageLog + '\n' + event.data;
+    });
+  };
 
   $http.get('/blacklist')
   .success(function(data) {
